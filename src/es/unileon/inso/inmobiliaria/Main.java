b2dbc2a5a360a7a777959217dd800bcf756e6c3d
tlemar00 <36696289+tlemar00@users.noreply.github.com>
2021-01-07 19:10:17 +0100

    interfaces

---
+++ src/es/unileon/inso/inmobiliaria/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package es.unileon.inso.inmobiliaria;
+
+/**
+ * @author tlemar00
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
